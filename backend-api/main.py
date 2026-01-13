8d5e3b2e9d49f5303f5ebf108f3f9f3498c1282e
maxtc <max995tt@gmail.com>
2026-01-13 02:21:02 +0000

    feat: add versions endpoint

--- backend-api/main.py
+++ backend-api/main.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI
 from api.api_v1 import api_router
 from fastapi.responses import RedirectResponse
 
-app = FastAPI(title="ToDo List API", version="1.0.0")
+app = FastAPI(title="ToDo List API", version="0.0.1")
 
 app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/")
 def read_root():
     return RedirectResponse(url="/docs")
+
+@app.get("/version")
+def get_version():
+    return {"version": app.version}
